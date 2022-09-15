a5947272934fe54ed7db9f48a3644b695dee4f67
Wilyanto <kada.indonesia@gmail.com>
2022-09-15 23:06:42 +0700

    added HeaderCartButton

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import { Fragment } from 'react';
+
 import Header from './components/Layout/Header';
 
 function App() {
   return (
-    <>
+    <Fragment>
       <Header />
-    </>
+    </Fragment>
   );
 }
 
